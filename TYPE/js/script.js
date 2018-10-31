14ac08323d9ade302f7bf2a87beb51926ee0d927
Andrea Gnanatheepan <agnanatheepan@gmail.com>
2018-10-31 10:25:50 -0400

    Type Project

---
+++ TYPE/js/script.js
@@ -0,0 +1,119 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var $dOut = $('#date'),
+    $hOut = $('#hours'),
+    $mOut = $('#minutes'),
+    $sOut = $('#seconds'),
+    $ampmOut = $('#ampm'),
+    $enterHit = 0;
+	var months = [
+	  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+	];
+
+	var days = [
+	  'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+	];
+
+	function update(){
+	  var date = new Date();
+	  
+	  var ampm = date.getHours() < 12
+	             ? 'AM'
+	             : 'PM';
+	  
+	  var hours = date.getHours() == 0
+	              ? 12
+	              : date.getHours() > 12
+	                ? date.getHours() - 12
+	                : date.getHours();
+	  
+	  var minutes = date.getMinutes() < 10 
+	                ? '0' + date.getMinutes() 
+	                : date.getMinutes();
+	  
+	  var seconds = date.getSeconds() < 10 
+	                ? '0' + date.getSeconds() 
+	                : date.getSeconds();
+	  
+	  var dayOfWeek = days[date.getDay()];
+	  var month = months[date.getMonth()];
+	  var day = date.getDate();
+	  var year = date.getFullYear();
+	  
+	  var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
+	  
+	  $dOut.text(dateString);
+	  $hOut.text(hours);
+	  $mOut.text(minutes);
+	  $sOut.text(seconds);
+	  $ampmOut.text(ampm);
+	} 
+
+	update();
+
+	window.setInterval(update, 1000);
+    $('h1.navigation').fadeOut(0);
+    $('h1.navigation').fadeIn(2500);
+
+    $(document).keydown(function(e){
+	//console.log(e.keyCode) - this shows what the key is for each button you press
+
+	//38 == upButton.
+	if (e.keyCode == 38) {
+	fs =$('.neon').css("font-size").replace("px","")
+	fs= parseInt(fs)+1	
+
+	$(".neon").css({
+		"font-size":fs+"px"
+	})
+
+	console.log(fs)
+	}
+
+	// 40 == downButton.
+ 	if (e.keyCode == 40) {
+		fs =$('.neon').css("font-size").replace("px","")
+		fs= parseInt(fs)-1	
+
+	$(".neon").css({
+		"font-size":fs+"px"	
+	})
+	
+	console.log(fs)
+	}
+
+	if (e.keyCode == 13) {
+	$enterHit = $enterHit+35;
+
+
+	if ($enterHit >= 255) {
+	$enterHit = 0
+	}
+
+	$(".neon").css('color', 'rgb(0,0,0,'+$enterHit+')');
+	//how do I make value go from white - fade to that blurred pink
+	console.log($enterHit)
+	} 
+
+	// 32 == spaceBar
+	if (e.keyCode == 32) {
+	$(".unit").css('font-size', '100%');
+	// how to toggle font-size with space bar? $('.unit').toggle();
+	} 
+
+	// 39 == rightArrow
+	if (e.keyCode == 39) {
+	$("#date").css({'background-color': '#FFF', 'color': '#000', 'opacity': '0.5'});
+	} 
+
+	// 37 == leftArrow
+	if (e.keyCode == 37) {
+	$("#date").css({'background-color': '#9F1E41', 'color': '#FFF', 'opacity': '0.5'});
+	} 
+
+
+})
+    });
+
+
+
