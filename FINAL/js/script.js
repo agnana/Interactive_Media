bc0fd145fc021bdfbe9d5ad5d24f383d37befc09
Andrea Gnanatheepan <agnanatheepan@gmail.com>
2018-11-30 18:06:47 -0500

    hi

--- FINAL/js/script.js
+++ FINAL/js/script.js
@@ -1,22 +1,31 @@
-$( document ).ready(function() {
-	console.log( "ready!" );
 
-      $("h2").click(function(){
+
+$( document ).ready(function() {
+
+    $("h2").click(function(){
        $("h2").fadeTo("medium", 0.50);
     });
-      $('.ovo').flickity({
-	  // options
-	  cellAlign: 'left',
-	  contain: true
-});
-      $("#toggle").click(function() {
+    
 
-  //on click, the hamburger menu elements get the toggle-active class and the overlay gets the nav-active class
-  $(this).toggleClass("toggle-active");
-  $("#overlay").toggleClass("nav-active");
-
-  //the h1 element toggles between visibility and invisibility
-  $("h1").toggleClass("hidden");
-});
+    $carousel =  $('.ovo').flickity({
+  	  // options
+  	  cellAlign: 'left',
+  	  contain: true
+    });
+    
+    $("#toggle").click(function() {
+        $(this).toggleClass("toggle-active"); //on click, the hamburger menu elements get the toggle-active class and the overlay gets the nav-active class
+        $("#overlay").toggleClass("nav-active");
+        $("h1").toggleClass("hidden"); //the h1 element toggles between visibility and invisibility
+    });
    
+
+    $(".button").click(function(){
+      i = $(this).attr("data-index")
+      $carousel.flickity( 'select', i )
+    })
+
+
 });
+
+
