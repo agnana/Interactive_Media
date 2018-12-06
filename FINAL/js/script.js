8da0240c7523eecb8e382ddab19096d9e9f8d66d
Andrea Gnanatheepan <agnanatheepan@gmail.com>
2018-12-05 19:50:15 -0500

    new

--- FINAL/js/script.js
+++ FINAL/js/script.js
@@ -1,29 +1,23 @@
 $( document ).ready(function() {
-
-    $(".ovo-artist-cell").click(function(){
-       $(".ovo-artist-cell").fadeTo("medium", 0.50);
-    });
-    
-
-    $carousel =  $('.ovo').flickity({
-  	  // options
-  	  cellAlign: 'left',
-  	  contain: true
-    });
-    
-    $("#toggle").click(function() {
-        $(this).toggleClass("toggle-active"); //on click, the hamburger menu elements get the toggle-active class and the overlay gets the nav-active class
-        $("#overlay").toggleClass("nav-active");
-        $("h1").toggleClass("hidden"); //the h1 element toggles between visibility and invisibility
-    });
-   
-
-    $(".button").click(function(){
-      i = $(this).attr("data-index")
-      $carousel.flickity( 'select', i )
-    })
-
-
+  $(".ovo-artist-cell").click(function(){
+  $(".ovo-artist-cell").fadeTo("medium", 0.50);
 });
 
+$carousel =  $('.ovo').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+$("#toggle").click(function() {
+  $(this).toggleClass("toggle-active"); //on click, the hamburger menu elements get the toggle-active class and the overlay gets the nav-active class
+  $("#overlay").toggleClass("nav-active");
 
+$("h1").toggleClass("hidden"); //the h1 element toggles between visibility and invisibility
+});
+
+$(".button").click(function(){
+i = $(this).attr("data-index")
+$carousel.flickity( 'select', i )
+})
+
+});
